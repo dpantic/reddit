e8968019b73e62066f3e04e4d16181df7fb03ded
Dejan <dpantic@gmail.com>
2012-02-20 16:53:28 +0100

    almost final version of homework2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Homework2::Application.config.secret_token = 'd6a76057e598fb3cc62cd29e5c96a737d58f114a4b52b732d6c8e5f8e008fb680aabff9ab47e054a41d49f0c6452e6efb8cc17fb8d42838b25c2e5bcfd3198f8'
